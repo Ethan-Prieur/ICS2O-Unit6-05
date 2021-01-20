a3a83e9534c705f61654f45d9c216ed93ad99848
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-20 16:02:40 +0000

    a lot of errors

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function myFunction () {
   age = parseInt(age)
   const day = document.getElementById('box2').value
   if ((age < 19) && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
-      alert('Get to School!')
-  } else if ((age > 18) && (day === 'Monday' || day == 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
-      alert('Get to Work!')
-  } else { 
-      alert('Time to Relax')
-  } 
+    alert('Get to School!')
+  } else if ((age > 18) && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
+    alert('Get to Work!')
+  } else {
+    alert('Time to Relax')
+  }
 }
