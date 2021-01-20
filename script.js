6f73e87b84484e314d1250976328ab4da38980d1
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-20 15:54:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('btn').addEventListener('click', myFunction)
+function myFunction () {
+  let age = document.getElementById('box1').value
+  age = parseInt(age)
+  const day = document.getElementById('box2').value
+  if ((age < 19) && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
+      alert('Get to School!')
+  } else if ((age > 18) && (day === 'Monday' || day == 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday')) {
+      alert('Get to Work!')
+  } else { 
+      alert('Time to Relax')
+  } 
+}
